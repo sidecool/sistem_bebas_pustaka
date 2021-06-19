ef0b22c7962313e7ec787ca7a5b74dc62d7d5298
sidecool <ahdie7@gmail.com>
2021-06-19 17:03:51 +0700

    commit

--- password/index.php
+++ password/index.php
@@ -10,33 +10,33 @@
                                 </div>
                                 <div class="card-body">
                                     <form method="post" action="" autocomplete="off" id="in-form">
-                                        <div class="form-group row">
-                                            <label for="paswd_old" class="col-sm-4 col-form-label text-right font-weight-bold">Password Lama</label>
+                                        <div class="row">
+                                            <label for="paswd_old" class="col-sm-4 col-form-label-sm text-right font-weight-bold">Password Lama</label>
                                             <div class="col-sm-8">
                                                 <div class="input-group mb-3">                                                    
-                                                    <input type="password" id="paswd_old" name="paswd_old" class="form-control" placeholder="Password Lama" required onkeydown="return f_cekenter(this, event)" tabIndex="1">
+                                                    <input type="password" id="paswd_old" name="paswd_old" class="form-control form-control-sm" placeholder="Password Lama" required onkeydown="return f_cekenter(this, event)" tabIndex="1">
                                                     <div class="input-group-append">
                                                         <button class="btn btn-outline-secondary" type="button" id="showOld"><i class="fa fa-eye-slash"></i></button>
                                                     </div>
                                                 </div>
                                             </div>                                            
                                         </div>
-                                        <div class="form-group row">
-                                            <label for="paswd_new" class="col-sm-4 col-form-label text-right font-weight-bold">Password Baru</label>
+                                        <div class="row">
+                                            <label for="paswd_new" class="col-sm-4 col-form-label-sm text-right font-weight-bold">Password Baru</label>
                                             <div class="col-sm-8">
                                                 <div class="input-group mb-3">
-                                                    <input type="password" id="paswd_new" name="paswd_new" class="form-control" placeholder="Password Baru" required onkeydown="return f_cekenter(this, event)" tabIndex="2">
+                                                    <input type="password" id="paswd_new" name="paswd_new" class="form-control form-control-sm" placeholder="Password Baru" required onkeydown="return f_cekenter(this, event)" tabIndex="2">
                                                     <div class="input-group-append">
                                                         <button class="btn btn-outline-secondary" type="button" id="showNew"><i class="fa fa-eye-slash"></i></button>
                                                     </div>
                                                 </div>    
                                             </div>                                            
                                         </div>
-                                        <div class="form-group row">
-                                            <label for="paswd_conf" class="col-sm-4 col-form-label text-right font-weight-bold">Re-type Password</label>
+                                        <div class="row">
+                                            <label for="paswd_conf" class="col-sm-4 col-form-label-sm text-right font-weight-bold">Re-type Password</label>
                                             <div class="col-sm-8">
                                                 <div class="input-group mb-3">
-                                                    <input type="password" id="paswd_conf" name="paswd_conf" class="form-control" placeholder="Re-type Password" required tabIndex="3">                                                    
+                                                    <input type="password" id="paswd_conf" name="paswd_conf" class="form-control form-control-sm" placeholder="Re-type Password" required tabIndex="3">                                                    
                                                     <div class="input-group-append">
                                                         <button class="btn btn-outline-secondary" type="button" id="showConf"><i class="fa fa-eye-slash"></i></button>
                                                     </div>
@@ -44,7 +44,7 @@
                                                 <p id="pesan"></p>
                                             </div>
                                         </div>
-                                        <div class="form-group row">
+                                        <div class="row">
                                             <div class="col-sm-4"></div>
                                             <div class="col-sm-8">
                                                 <button class="btn btn-sm btn-primary" type="submit" id="simpan" form="in-form" onclick="return validatePaswdConf()"><i class="fa fa-save"></i><span> Simpan</span></button>
